d25fd8ae285e0cedb7353b76923c6b7ec7a535f6
meicib <ameicib@berkeley.edu>
2026-02-24 12:52:19 -0800

    Update enrollment page for web

--- src/components/EnrollmentInfoPage/EnrollmentInfo.js
+++ src/components/EnrollmentInfoPage/EnrollmentInfo.js
@@ -21,18 +21,20 @@ function EnrollmentInfoPage() {
         <h1 style={{"color":"#3e9ec3", "fontWeight":"bold", "fontSize":"2.5em"}}>How do I Enroll?</h1>
       </div>
 
-      <p style={{"textAlign":"center", "color":"#3e9ec3", "fontWeight":"500", "letterSpacing":"-0.01em" }}>Enroll in Cubstart through these 4 easy steps!</p>
+      <p style={{"textAlign":"center", "color":"#3e9ec3", "fontWeight":"500", "letterSpacing":"-0.01em" }}>Enroll in Cubstart through these 2 easy steps!</p>
 
       <br/>
 
-      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 1: </h3><p style={{"marginTop":"19px"}}>Go to "Enrollment Center" in <a style={{"cursor":"pointer", "borderBottom":"1px solid #3e9ec3", "fontWeight":"600",}} target="_blank" href="https://calcentral.berkeley.edu/academics">CalCentral</a>.</p></div>
+      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 1: </h3><p style={{"marginTop":"19px"}}>
+        Join the <a style={{"cursor":"pointer", "borderBottom":"1px solid #3e9ec3", "fontWeight":"600",}} target="_blank" href="https://www.gradescope.com">Gradescope</a>with code WNK77J.</p>
+      </div>
+      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 1: </h3><p style={{"marginTop":"19px"}}>
+        Berkeley students: Join the <a style={{"cursor":"pointer", "borderBottom":"1px solid #3e9ec3", "fontWeight":"600",}} target="_blank" href="https://edstem.org/us/join/sSTJwY">EdStem</a>.</p>
+      </div>
+      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 1: </h3><p style={{"marginTop":"19px"}}>
+        non-Berkeley students: Fill out <a style={{"cursor":"pointer", "borderBottom":"1px solid #3e9ec3", "fontWeight":"600",}} target="_blank" href="https://forms.gle/fQV6pXzRH3Kkkxsm9">this form</a> to be added manually to the EdStem.</p>
+      </div>
       
-      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px", flexShrink: "0" }}>Step 2: </h3><p style={{"marginTop":"19px"}}>Search for either <span style={{"color":"#3e9ec3", "fontWeight":"600", "letterSpacing":"0em"}}>30193</span> for our <span style={{"color":"#3e9ec3", "fontWeight":"600", "letterSpacing":"0em"}}>Web Track</span> (CS 198-750) or <span style={{"color":"#3e9ec3", "fontWeight":"600", "letterSpacing":"0em"}}>30192</span> for our <span style={{"color":"#3e9ec3", "fontWeight":"600", "letterSpacing":"0em"}}>iOS Track</span> (CS 198-075). Please note the web section will only be meeting on Tuesdays from 6:30pm to 8pm.</p></div>
-      
-      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 3: </h3><p style={{"marginTop":"19px"}}>Go through all the steps to enroll, leaving the permission number blank!</p></div>
-
-      <div style={{ "display":"flex", "flexDirection":"row", "alignItems":"center"}}><h3 className="enrollment_header" style={{"color":"#3e9ec3", "marginRight":"10px",}}>Step 4: </h3><p style={{"marginTop":"19px"}}> Join your respective section's Ed and Gradescope in the "Schedule" Tab in the Cubstart Website. <br></br> Then you're <span style={{"color":"#3e9ec3", "fontWeight":"700", "letterSpacing":"-0.03em"}}>DONE!</span></p></div>
-    
       <p>Got stuck? Ask us anything at <a href="mailto:team@cubstart.com" style={{"color":"#3e9ec3", "fontWeight":"600", "letterSpacing":"0em"}}>team@cubstart.com</a></p>
       
     </div>
